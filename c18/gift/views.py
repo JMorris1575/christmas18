2bdef0e7a1506721d4a9ea3b0ae8d0de18008bc8
JMorris1575 <FrJamesMorris@gmail.com>
2018-11-07 21:08:01 -0500

    Initial draft of header.html

--- c18/gift/views.py
+++ c18/gift/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 class GiftListView(View):
-    pass
+    template = 'gift.temp.html'
+
+    def get(self, request):
+        return render(request, self.template)
