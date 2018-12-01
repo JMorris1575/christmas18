1cae76bddcf266716eae9480c6fb233cde476311
JMorris1575 <FrJamesMorris@gmail.com>
2018-11-30 21:33:51 -0500

    Finished with memory app... for now anyway.

--- c18/memory/urls.py
+++ c18/memory/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import RedirectView
 from django.urls import reverse_lazy
 
-from .views import MemoryListView, MemoryEditView
+from .views import MemoryListView, MemoryEditView, MemoryDeleteView
 
 app_name = 'memory'
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', RedirectView.as_view(url=reverse_lazy('memory:memory_list'))),
     path('collection/', login_required(MemoryListView.as_view()), name='memory_list'),
     path('create/', login_required(MemoryEditView.as_view()), name='create'),
-    path('edit/<int:memory_id>', login_required(MemoryEditView.as_view()), name='edit')
+    path('edit/<int:memory_id>', login_required(MemoryEditView.as_view()), name='edit'),
+    path('delete/<int:memory_id>', login_required(MemoryDeleteView.as_view()), name='delete'),
 ]
